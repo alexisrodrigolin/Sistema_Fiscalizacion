fb1111934decde99a53621609e6244c9a027a8c0
alexisrodrigolin <alexisrodrigolin@gmail.com>
2025-01-06 20:31:52 -0300

    01

--- PriceSyncer/mainPrice.py
+++ PriceSyncer/mainPrice.py
@@ -8,7 +8,8 @@ class main():
         self.app= tb.Window(themename= "darkly",title= "PriceSyncer",size= [1024, 768])
 
         self.create_widget_menu()
-        self.columnas_rows(col=8, row=15)
+        self.settings(col=8, row=15)
+
         self.app.mainloop()
     def create_widget_menu(self):
         self.numberCaja= tb.Label(self.app, text="PriceSyncer", font=("helveica",35,"italic","bold"),anchor="center")
@@ -21,18 +22,21 @@ class main():
         self.Password.grid(column=3, row=6,sticky= "n")
         self.User.grid(column=3, row=5)
         self.numberCaja.grid(column=3,  row=3)  
+
+
+
     def check(self, event=None):
         username = self.User.get()  
         password= self.Password.get()
         if self.Back.validate_user(username,password):
-            self.pay= tb.Button(self.app,
-            text="PayPoint", )
-            self.sales= tb.Button(self.app,
-            text = "SalesOverview", )
+            self.precio= tb.Button(self.app,
+            text="Precio", style= "darkly")
+            self.etiquetas= tb.Button(self.app,
+            text = "etiquetas", style= "darkly")
             
         
-            self.sales.grid(column=3, row=11)
-            self.pay.grid(column=3,row=10)
+            self.etiquetas.grid(column=3, row=11, )
+            self.precio.grid(column=3,row=10, )
             try:
                 self.error.grid_forget()
             except:
@@ -44,13 +48,14 @@ class main():
             foreground= "red" )
             self.error.grid(column=3, row=6, sticky= "s")
             try:
-                self.pay.grid_forget()
-                self.sales.grid_forget()
+                self.precio.grid_forget()
+                self.etiquetas.grid_forget()
             except:
                 pass
 
-    def columnas_rows(self, col, row):
+    def settings(self, col, row):
         "columnas y rows asignados"
+        self.app.bind("<Return>", self.check)
 
         for i in range(col):
             self.app.columnconfigure(i,weight= 1)
@@ -58,11 +63,9 @@ class main():
         for z in range(row):
             self.app.rowconfigure(z,weight=1)
 
+
+
         
 
-        self.enter.grid(column=3,row=7)
-        self.Password.grid(column=3, row=6,sticky= "n")
-        self.User.grid(column=3, row=5)
-        self.numberCaja.grid(column=3,  row=3)   
 main()
         
